0816fac738c80fb42b3f2b8f9961e6f27d80cbec
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-08-07 13:42:05 +0200

    starting

--- fast_optimization/__init__.py
+++ fast_optimization/__init__.py
@@ -1,5 +1,7 @@
 # src/__init__.py
 
 # Import modules and functions from your package here
-from .objectives_functions import objective_functions, mielke_skill_score
-from .setup_spotpy import setup_spotpy
+from .objectives_functions import objective_functions, multi_obj_func
+from .metrics import *
+from .SPEA2 import spea2_algorithm
+from .NSGAII import nsgaii_algorithm, nsgaii_algorithm_ts
