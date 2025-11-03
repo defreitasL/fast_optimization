be0e921684f2d8f7aa61458c4f65ddf3b5a482fc
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-11-03 17:42:23 +0100

    Trying EnKF

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fast_optimization',
-    version='1.1.23',
+    version='1.1.24',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
