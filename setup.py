5ee827fd2409fd51c5c751d8ac213f1826d8b5a9
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-12-02 16:25:25 +0100

    Adding hybrid

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fast_optimization',
-    version='1.1.33',
+    version='1.1.34',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
